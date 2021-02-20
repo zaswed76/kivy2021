deef2cd635ee37114f72b191b6c0abd331d8b191
zaswed76 <zaswed76@gmail.com>
2021-02-20 20:26:38 +0200

    start

--- firstkv/main.py
+++ firstkv/main.py
@@ -1,13 +1,15 @@
 from kivy.app import App
 from kivy.uix.label import Label
+from kivy.uix.button import Button
+from kivy.uix.boxlayout import BoxLayout
+
+class Container(BoxLayout):
+    pass
 
 class MainApp(App):
     def build(self):
-        label = Label(text='Hello from Kivy',
-                      size_hint=(.5, .5),
-                      pos_hint={'center_x': .5, 'center_y': .5})
+        return Container()
 
-        return label
 
 if __name__ == '__main__':
     app = MainApp()

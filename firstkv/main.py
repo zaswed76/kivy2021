a01a09da976d53b199306786d3aeaff0bcd54960
zaswed76 <zaswed76@gmail.com>
2021-02-21 16:54:46 +0200

    test lesson

--- firstkv/main.py
+++ firstkv/main.py
@@ -1,10 +1,36 @@
 from kivy.app import App
-from kivy.uix.label import Label
-from kivy.uix.button import Button
-from kivy.uix.boxlayout import BoxLayout
+from kivy.config import Config
+from kivy.core.window import Window
+from kivy.uix.gridlayout import GridLayout
 
-class Container(BoxLayout):
-    pass
+Config.set('kivy', 'keyboard_mode', 'systemanddock')
+
+# Window.size = (480, 853)
+
+
+def get_ingridients(m):
+    nitro = str(10 * m / 1000)
+    salt = str(15 * m / 1000)
+    starts = str(0.5 * m / 1000)
+    dextstroze = str(5 * m / 1000)
+    setting_time = str(round(m / 500 * 2))
+    return dict(nitro=nitro, salt=salt, starts=starts, dextstroze=dextstroze,
+                setting_time=setting_time)
+
+
+class Container(GridLayout):
+    def calculate(self):
+        try:
+            mass = int(self.text_input.text)
+        except:
+            mass = 0
+        ingridients = get_ingridients(mass)
+        self.salt.text = ingridients.get("salt")
+        self.nitro.text = ingridients.get("nitro")
+        self.sugar.text = ingridients.get("dextstroze")
+        self.starts.text = ingridients.get("starts")
+        self.time.text = ingridients.get("setting_time")
+
 
 class FirstCatalogApp(App):
     def build(self):
